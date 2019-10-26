244a46d9350c950aa1447b0bab90996d9f28e273
googlebabaio <51003033@qq.com>
2019-10-26 08:51:22 +0800

    修改一些问题

--- tool-config.sh
+++ tool-config.sh
@@ -8,6 +8,5 @@ configKubectlAutoSpell(){
 
 configNStool(){
   git clone https://github.com/ahmetb/kubectx /opt/kubectx
-  ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
   ln -s /opt/kubectx/kubens /usr/local/bin/ns
 }
